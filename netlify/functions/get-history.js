3e49448f91120917e03ab424f2d2013579bb1af6
Nico Yonathan <sapikurus@gmail.com>
2026-07-31 11:37:32 +0700

    v15: deploy Netlify functions + graceful travel empty state

--- netlify/functions/get-history.js
+++ netlify/functions/get-history.js
@@ -6,6 +6,7 @@ export default async (req) => {
   const store = getStore('sumbawamba');
   let hist;
   try{ hist = await store.get('history',{type:'json'}) }catch(e){ hist=null }
-  if(!hist) hist = {travel:[],buys:[],lastTs:0,updated:0};
+  if(!hist) hist = {travel:[],buys:[],stocks:[],lastTs:0,lastStockTs:0,updated:0};
+  if(!hist.stocks) hist.stocks=[];
   return new Response(JSON.stringify(hist),{headers:{'Content-Type':'application/json','Cache-Control':'no-cache'}});
 };
